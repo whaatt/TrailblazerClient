c635a1d1ddce49707cd2208cfc54a47e2be62ae3
Sanjay Kannan <whaatt@gmail.com>
2013-07-09 02:09:42 -0400

    Integrate FootPath by RWTH Aachen.

--- src/com/research/siemens/trailblazer/MainActivity.java
+++ src/com/research/siemens/trailblazer/MainActivity.java
@@ -1,112 +1,122 @@
 package com.research.siemens.trailblazer;
 
 import android.app.Activity;
-import android.content.Context;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import de.uvwxy.footpath.core.StepDetection;
+import de.uvwxy.footpath.core.StepTrigger;
 
-public class MainActivity extends Activity implements SensorEventListener {
-    private SensorManager mSensorManager;
-    private Sensor mAccelerometer;
+public class MainActivity extends Activity implements StepTrigger {
 
-    private int iterations = 0;
+    public static final String CALIBRATION = "CALIBRATION"; //the name of our sharedPreferences file
 
-    private double dt = 0;
-    private long newStamp = 0;
-    private long lastStamp = System.currentTimeMillis();
+    float peak;			// threshold for step detection
+    float alpha;		// value for low pass filter
+    int stepTimeoutM;	// distance in ms between each step
 
-    //initial accelerations
-    private double accX = 0;
-    private double accY = 0;
-    private double accZ = 0;
+    double initHead = -1;    // note down initial heading
 
-    //initial velocities
-    private double velX = 0;
-    private double velY = 0;
-    private double velZ = 0;
+    double locX = 0;
+    double locY = 0;
+    double locZ = 0;
 
-    //initial positions
-    private double locX = 0;
-    private double locY = 0;
-    private double locZ = 0;
+    StepDetection stepDetection; // global step detector
 
     /**
-     * Called when the activity is first created.
+     * Activity life cycle.
      */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     protected void onResume() {
         super.onResume();
-        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     protected void onPause() {
         super.onPause();
-        mSensorManager.unregisterListener(this);
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        //ignore this at the moment
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        TextView status = (TextView)findViewById(R.id.status);
-
-        double[] gravity = new double[3];
-        final double alpha = 0.9;
-
-        gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
-        gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
-        gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
-
-        newStamp = System.currentTimeMillis();
-        dt = (float) (newStamp - lastStamp) / (float) 1000;
-        lastStamp = newStamp;
-
-        accX = event.values[0] - gravity[0];
-        accY = event.values[1] - gravity[1];
-        accZ = event.values[2] - gravity[2];
-
-        //accX = event.values[0];
-        //accY = event.values[1];
-        //accZ = event.values[2];
-
-        if (iterations > 10){
-            velX = velX + accX * dt;
-            velY = velY + accY * dt;
-            velZ = velZ + accZ * dt;
-
-            locX = locX + velX * dt;
-            locY = locY + velY * dt;
-            locZ = locZ + velZ * dt;
-        }
-
-        iterations++;
-
-        status.setText("Acc: (" + Double.toString(accX) + ", " + Double.toString(accY) + ", " + Double.toString(dt) + ")\n"
-            + "Vel: (" + Double.toString(velX) + ", " + Double.toString(velY) + ", " + Long.toString(newStamp) + ")\n"
-            +"Loc: (" + Double.toString(locX) + ", " + Double.toString(locY) + ", " + Long.toString(lastStamp) + ")");
     }
 
     /**
      * Called when the exit button is clicked.
      */
+
     public void exitButton(View v) {
         finish();
+    }
+
+    /**
+     * Called when the start button is clicked.
+     */
+
+    public void startButton(View v) {
+        //load settings and create step detector
+        loadSettings();
+        stepDetection = new StepDetection(this, this, alpha, peak, stepTimeoutM);
+        stepDetection.load();
+
+        TextView status = (TextView) findViewById(R.id.status);
+        status.setText("Waiting for movement.");
+    }
+
+    /**
+     * Load settings from sharedPreferences.
+     */
+
+    private void loadSettings(){
+        alpha = getSharedPreferences(CALIBRATION, 0).getFloat("a", 0.5f);
+        peak = getSharedPreferences(CALIBRATION, 0).getFloat("peak", 0.5f);
+        stepTimeoutM = getSharedPreferences(CALIBRATION, 0).getInt("timeout", 666);
+    }
+
+    /**
+     * Methods that implement StepTrigger from FootPath.
+     */
+
+    @Override
+    public void trigger(long nowMS, double compDir) {
+        //must be declared final to use in anon class
+        final long nowMSF = nowMS;
+        final double compDirF = compDir;
+
+        runOnUiThread(new Runnable() {
+            public void run() {
+                onStep(nowMSF, compDirF);
+            }
+        });
+    }
+
+    //Actually handle trigger steps.
+    public void onStep(long nowMS, double compDir){
+        if (initHead == -1){
+            initHead = compDir;
+        }
+
+        double heading = Math.toRadians(compDir - initHead); //heading is relative to original
+        locX += (double) 1 * Math.cos(heading); //lol this says stride length = 1 meter
+        locY += (double) 1 * Math.sin(heading);
+
+        TextView status = (TextView) findViewById(R.id.status);
+        status.setText("Heading: " + Double.toString(compDir) + "\nTime: " + Long.toString(nowMS)
+            + "\n\nX: " + Double.toString(locX) + "\nY: " + Double.toString(locY));
+    }
+
+    @Override
+    public void dataHookAcc(long now_ms, double x, double y, double z) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void dataHookComp(long now_ms, double x, double y, double z) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void timedDataHook(long now_ms, double[] acc, double[] comp) {
+        //To change body of implemented methods use File | Settings | File Templates.
     }
 }
